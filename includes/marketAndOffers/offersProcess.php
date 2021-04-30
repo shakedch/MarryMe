9f1ac5fdbaf81cc5d6bcc6a4b00146b74348f4cf
shakedch <69304551+shakedch@users.noreply.github.com>
2021-04-30 22:42:33 +0300

    Upload, update and delete offers image

--- includes/marketAndOffers/offersProcess.php
+++ includes/marketAndOffers/offersProcess.php
@@ -20,8 +20,9 @@ if (isset($_POST['save'])) {
     $valid_date =  $_POST['valid_date'];
     $description =  $_POST['description'];
     $price =  $_POST['price'];
-    $img =  $_POST['img'];
     $vendor_id =  2;/*צריך לבדוק איך נותנים לזה סשן ואת מי שמחובר */
+
+    $img = uploadImg();
 
     $mysqli->query("INSERT INTO offers (valid_date, name, description, img, price, vendor_id) VALUES ('$valid_date', '$name', '$description', '$img', '$price', '$vendor_id')") or die($mysqli->error);
 
@@ -34,6 +35,18 @@ if (isset($_POST['save'])) {
 //delete form
 if (isset($_GET['delete'])) {
     $offer_id = $_GET['delete'];
+
+    //delete record image
+    $result =  $mysqli->query("SELECT * FROM offers WHERE offer_id=$offer_id") or die($mysqli->error);
+    $row = $result->fetch_array();
+    $temp_img_to_delete = $row['img'];
+
+    //delete image if exist
+    if ($temp_img_to_delete !== '') {
+        // Use unlink() function to delete a file 
+        $file_pointer = "../../assets/img/offersUploads/" . $temp_img_to_delete;
+        unlink($file_pointer);
+    }
 
     $mysqli->query("DELETE FROM offers WHERE offer_id=$offer_id") or die(mysqli_error(($mysqli)));
 
@@ -61,20 +74,70 @@ if (isset($_GET['edit'])) {
 
 //update form
 if (isset($_POST['update'])) {
-
     $offer_id = $_POST['offer_id'];
     $name = $_POST['name'];
     $valid_date =  $_POST['valid_date'];
     $description =  $_POST['description'];
     $price =  $_POST['price'];
-    $img =  $_POST['img']; // צריך לטפל בעדכון תמונה
-    $vendor_id =  1;/*צריך לבדוק איך נותנים לזה סשן ואת מי שמחובר */
+    $vendor_id =  2;/*צריך לבדוק איך נותנים לזה סשן ואת מי שמחובר */
 
-    echo "<script type='text/javascript'>alert('$description');</script>";
+    $result =  $mysqli->query("SELECT * FROM offers WHERE offer_id=$offer_id") or die($mysqli->error);
+    $row = $result->fetch_array();
+    $temp_img_to_delete = $row['img'];
 
-    $mysqli->query("UPDATE offers SET name='$name',valid_date='$valid_date',description='$description',price='$price',img='$img',vendor_id='$vendor_id' WHERE offer_id=$offer_id ") or die($mysqli->error);
+    $img = uploadImg();
+
+    //If there is no new image
+    if ($img === '') {
+        $mysqli->query("UPDATE offers SET name='$name',valid_date='$valid_date',description='$description',price='$price',vendor_id='$vendor_id' WHERE offer_id=$offer_id ") or die($mysqli->error);
+    } else {
+
+        //delete prev image if exist
+        if ($temp_img_to_delete !== '') {
+            // Use unlink() function to delete a file 
+            $file_pointer = "../../assets/img/offersUploads/" . $temp_img_to_delete;
+            unlink($file_pointer);
+        }
+
+        $mysqli->query("UPDATE offers SET name='$name',valid_date='$valid_date',description='$description',price='$price',img='$img',vendor_id='$vendor_id' WHERE offer_id=$offer_id ") or die($mysqli->error);
+    }
+
     $_SESSION['message'] = "offer has been updated!";
     $_SESSION['msg_type'] = "warning";
 
     header("location: offers.php");
+}
+
+function uploadImg()
+{
+    $cleanName = '';
+    //Uploading image
+    $filePath = pathinfo($_FILES['img']['name']);
+    $ext = $filePath['extension']; // get the extension of the file
+    $cleanName = $filePath['filename']; //get the name without extention
+
+    // In case of new image
+    if ($cleanName !== '') {
+
+        //Check existing file names to avoid overwriting files
+        $originalName = $cleanName;
+        $num = 1;
+        while (file_exists("../../assets/img/offersUploads/" . $cleanName . "." . $ext)) {
+            $cleanName = (string)$originalName . "_" . $num;
+            $num++;
+        }   // Add a running number if the file name already exists
+        $newName = $cleanName . "." . $ext;
+
+
+        $target = '../../assets/img/offersUploads/' . $newName;
+
+        if (move_uploaded_file($_FILES["img"]["tmp_name"], $target)) {
+            echo "The photo has been uploaded.";
+        } else {
+            echo "There was an error uploading the photo.";
+        }
+
+        return $newName;
+    }
+    return $cleanName;
 }
