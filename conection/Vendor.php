b2cdccc16df28e3a6db520928c465fb903749fc4
Sapir Koren <sapirke88@gmail.com>
2021-05-15 16:47:40 +0300

    [feat-19] added css to alerts and changed position of the alert

--- conection/Vendor.php
+++ conection/Vendor.php
@@ -3,152 +3,158 @@
 require_once('init.php');
 
 // Building a class for Vendor
-class Vendor{
-	//Class variables
-	private $vendor_id;
+class Vendor
+{
+    //Class variables
+    private $vendor_id;
     private $email;
-	private $password;
-	private $company_name;
-	private $phone_num;
-	private $kind_of_business;
-	private $web_url;
+    private $password;
+    private $company_name;
+    private $phone_num;
+    private $kind_of_business;
+    private $web_url;
     private $address;
-	private $role;
-    
-	// A function that takes all the record in a Vendor table and puts them into an array of objects
-    public static function fetch_users(){
-        
+    private $role;
+
+    // A function that takes all the record in a Vendor table and puts them into an array of objects
+    public static function fetch_users()
+    {
+
         global $database;
-        $result=$database->query("select * from vendors");
-        $vendors=null;
-        if ($result){
-            $i=0;
-            if ($result->num_rows>0){ 
-                while($row=$result->fetch_assoc()){ 
-                    $vendor=new Vendor();
+        $result = $database->query("select * from vendors");
+        $vendors = null;
+        if ($result) {
+            $i = 0;
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $vendor = new Vendor();
                     $vendor->instantation($row);
-                    $vendors[$i]=$vendor;
-                    $i+=1;
+                    $vendors[$i] = $vendor;
+                    $i += 1;
                 }
             }
         }
         return $vendors;
     }
-     // Two functions that make the record an object      
-    private function has_attribute($attribute){
-        
-        $object_properties=get_object_vars($this);
-        return array_key_exists($attribute,$object_properties);
-    }
-    
-     private function  instantation($user_array){
-        foreach ($user_array as $attribute=>$value){
-            if ($result=$this->has_attribute($attribute))
-                $this->$attribute=$value;
-       }
-     }
-	 
-	// Function that finds a Vendor by email and password
-	public function find_user_by_name($email,$password){
-        global $database;
-        $error=null;	
-        $result=$database->query("select * from vendors where email='".$email."' and password='".$password."'");
-        if (!$result)
-            $error='Cant not find the user.  Error is:'.$database->get_connection()->error;
-        elseif ($result->num_rows>0){
-            $found_user=$result->fetch_assoc();
-			$this->instantation($found_user);
-        }
-         else
-             $error="Can no find user by this Email";
-		 
-        return $error;
+    // Two functions that make the record an object      
+    private function has_attribute($attribute)
+    {
 
+        $object_properties = get_object_vars($this);
+        return array_key_exists($attribute, $object_properties);
     }
-	//Function that finds a Vendor by email
-	public function find_email($email){
+
+    private function  instantation($user_array)
+    {
+        foreach ($user_array as $attribute => $value) {
+            if ($result = $this->has_attribute($attribute))
+                $this->$attribute = $value;
+        }
+    }
+
+    // Function that finds a Vendor by email and password
+    public function find_user_by_name($email, $password)
+    {
         global $database;
-        $error=null;
-        $result=$database->query("select * from vendors where email='".$email."'");
+        $error = null;
+        $result = $database->query("select * from vendors where email='" . $email . "' and password='" . $password . "'");
         if (!$result)
-            $error=null;
-        elseif ($result->num_rows>0){
+            $error = 'Cannot find the user.  Error is:' . $database->get_connection()->error;
+        elseif ($result->num_rows > 0) {
+            $found_user = $result->fetch_assoc();
+            $this->instantation($found_user);
+        } else
+            $error = "Cannot find user by this Email";
+
+        return $error;
+    }
+    //Function that finds a Vendor by email
+    public function find_email($email)
+    {
+        global $database;
+        $error = null;
+        $result = $database->query("select * from vendors where email='" . $email . "'");
+        if (!$result)
+            $error = null;
+        elseif ($result->num_rows > 0) {
             $error = 'error';
-        }
-         else
-             $error=null;
-		 
+        } else
+            $error = null;
+
         return $error;
-	}
-	// Function that finds a Vendor by vendor_id
-		public function find_user_by_id($id){
+    }
+    // Function that finds a Vendor by vendor_id
+    public function find_user_by_id($id)
+    {
         global $database;
-        $error=null;
-        $result=$database->query("select * from vendors where vendor_id='".$id."'");
+        $error = null;
+        $result = $database->query("select * from vendors where vendor_id='" . $id . "'");
         if (!$result)
-            $error='Can not find the vendor.  Error is:'.$database->get_connection()->error;
-        elseif ($result->num_rows>0){
-            $found_user=$result->fetch_assoc();
-			$this->instantation($found_user);
-        }
-         else
-             $error="Can no find vendor by this id";
-		 
+            $error = 'Cannot find the vendor.  Error is:' . $database->get_connection()->error;
+        elseif ($result->num_rows > 0) {
+            $found_user = $result->fetch_assoc();
+            $this->instantation($found_user);
+        } else
+            $error = "Cannot find vendor by this id";
+
         return $error;
-	}
-	// Function that finds a Vendor by vendor_id and give yes or no
-	public function find_vendor($id){
+    }
+    // Function that finds a Vendor by vendor_id and give yes or no
+    public function find_vendor($id)
+    {
         global $database;
-        $error=null;
-        $result=$database->query("select * from vendors where vendor_id='".$id."'");
+        $error = null;
+        $result = $database->query("select * from vendors where vendor_id='" . $id . "'");
         if (!$result)
-            $error=null;
-        elseif ($result->num_rows>0){
+            $error = null;
+        elseif ($result->num_rows > 0) {
             $error = 'error';
-        }
-         else
-             $error=null;
-		 
+        } else
+            $error = null;
+
         return $error;
-	}
-	//Function that finds a Vendor by email
-	public function find_user_by_email($email){
+    }
+    //Function that finds a Vendor by email
+    public function find_user_by_email($email)
+    {
         global $database;
-        $error=null;
-        $result=$database->query("select * from vendors where email='".$email."'");
+        $error = null;
+        $result = $database->query("select * from vendors where email='" . $email . "'");
         if (!$result)
-            $error='Can not find the user.  Error is:'.$database->get_connection()->error;
-        elseif ($result->num_rows>0){
-            $found_user=$result->fetch_assoc();
-			$this->instantation($found_user);
-        }
-         else
-             $error="Can no find user by this Email";
-		 
+            $error = 'Cannot find the user.  Error is:' . $database->get_connection()->error;
+        elseif ($result->num_rows > 0) {
+            $found_user = $result->fetch_assoc();
+            $this->instantation($found_user);
+        } else
+            $error = "Cannot find user by this Email";
+
         return $error;
-	}
-	//Function that add new Vendor
-    public static function add_Vendor($email,$password,$company_name,$phone_num,$kind_of_business,$web_url,$address){
+    }
+    //Function that add new Vendor
+    public static function add_Vendor($email, $password, $company_name, $phone_num, $kind_of_business, $web_url, $address)
+    {
         global $database;
-        $error=null;
-		$Save_Password=md5($password);
-		$role = 'Vendor';
-        $sql="Insert into vendors(email,password,company_name,phone_num,kind_of_business,web_url,address,role) values ('".$email."','".$Save_Password."','".$company_name."','".$phone_num."','".$kind_of_business."','".$web_url."','".$address."','".$role."')";
-        $result=$database->query($sql);
+        $error = null;
+        $Save_Password = md5($password);
+        $role = 'Vendor';
+        $sql = "Insert into vendors(email,password,company_name,phone_num,kind_of_business,web_url,address,role) values ('" . $email . "','" . $Save_Password . "','" . $company_name . "','" . $phone_num . "','" . $kind_of_business . "','" . $web_url . "','" . $address . "','" . $role . "')";
+        $result = $database->query($sql);
         if (!$result)
-            $error='Can not add user.  Error is:'.$database->get_connection()->error;
-        return $error;  
+            $error = 'Cannot add user.  Error is:' . $database->get_connection()->error;
+        return $error;
     }
-	//Function that update_data for Vendor
-	public function update_data($email,$company_name,$phone_num,$kind_of_business,$web_url,$address){
+    //Function that update_data for Vendor
+    public function update_data($email, $company_name, $phone_num, $kind_of_business, $web_url, $address)
+    {
         global $database;
-        $error=null;
-        $result=$database->query("UPDATE `vendors` SET `company_name` ='".$company_name."',`phone_num` ='".$phone_num."',`kind_of_business` ='".$kind_of_business."',`web_url` ='".$web_url."',`address` ='".$address."' WHERE `email`='".$email."'");
-        return $error;  
+        $error = null;
+        $result = $database->query("UPDATE `vendors` SET `company_name` ='" . $company_name . "',`phone_num` ='" . $phone_num . "',`kind_of_business` ='" . $kind_of_business . "',`web_url` ='" . $web_url . "',`address` ='" . $address . "' WHERE `email`='" . $email . "'");
+        return $error;
     }
-	// get function
-    public function __get($property){
-        if (property_exists($this,$property))
+    // get function
+    public function __get($property)
+    {
+        if (property_exists($this, $property))
             return $this->$property;
-    }  
+    }
 }
