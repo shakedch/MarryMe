0fef3640b0af6c535d6b525424678b19fb9ddfbf
Sapir Koren <sapirke88@gmail.com>
2021-05-18 20:45:10 +0300

    [feat-23] update design of registartion forms

--- includes/usersManagment/signUpUser.php
+++ includes/usersManagment/signUpUser.php
@@ -37,57 +37,97 @@ if (isset($_POST['submit'])) {
     }
 }
 ?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <!-- connection css and bootstrap and metadata -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Registration Form for couples</title>
-    <link rel="stylesheet" type="text/css" href="../../css/signup.css">
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript">
+    </script>
+    <script src="https://kit.fontawesome.com/90569433a0.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+    <!-- BS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    </script>
+    <!-- BS -->
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    </script>
+    <!-- tab view -->
     <link rel="shortcut icon" href="../../assets/img/tab_logo.png" type="image/png">
+    <title> Registration Couple</title>
+    <!-- general fonts-->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,600,700&amp;display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&family=Josefin+Sans:wght@500&family=Niconne&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="../../css/general.css" />
+    <link rel="stylesheet" href="../../css/signUp.css" />
 </head>
 
 <body>
-    <p id="error"><?php echo $error ?></p>
-    <div class="login-box">
-        <h1>Registration - Couples</h1>
-        <!-- form for Registration  -->
-        <form method="post">
-            <div class="textbox">
-                <i class="fa fa-envelope" aria-hidden="true"></i>
-                <input type="email" placeholder="Example@gmail.com" name="email" required>
+    <main class="d-flex align-items-center min-vh-100 py-3 py-md-0">
+        <div class="container">
+            <div class="card registerForm-card">
+                <div class="row no-gutters">
+                    <div class="col-md-5">
+                        <img src="../../assets/img/signUpUser.jpg" alt="registerForm" class="registerForm-card-img">
+                    </div>
+                    <div class="col-md-7">
+                        <div class="card-body">
+                            <p class="registerForm-card-description">Registration For The Happy Couple</p>
+                            <!-- form for log in  -->
+                            <form method="post">
+                                <div class="form-group">
+                                    <input class="form-control" type="email" placeholder="Example@gmail.com"
+                                        name="email" required>
+                                </div>
+                                <div class="form-group mb-4">
+                                    <input class="form-control" type="password" placeholder="password" name="password"
+                                        required>
+                                </div>
+                                <div class="form-group mb-4">
+                                    <input class="form-control" type="text" placeholder="Enter first full name"
+                                        name="full_name1" required>
+                                </div>
+                                <div class="form-group mb-4">
+                                    <input class="form-control" type="text" placeholder="Enter second full name"
+                                        name="full_name2" required>
+                                </div>
+                                <div class="form-group mb-4">
+                                    <input class="form-control" type="date" placeholder="Enter Wedding Date"
+                                        name="date_of_wedding" required>
+                                </div>
+                                <div class="form-group mb-4">
+                                    <input class="form-control" type="time" placeholder="Enter hour of wedding"
+                                        name="hour_of_wedding" required>
+                                </div>
+                                <div class="form-group mb-4">
+                                    <input class="form-control" type="text" placeholder="What is your budget?"
+                                        name="budget" required>
+                                </div>
+                                <p id="error"><?php echo $error ?></p>
+                                <input class="btn btn-block registerForm-btn mb-4" type="submit" name="submit"
+                                    value="Registration">
+                            </form>
+                            <p> Already have an account? <a href="login.php">Sign-In</a></p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="textbox">
-                <i class="fa fa-lock" aria-hidden="true"></i>
-                <input type="password" placeholder="password" name="password" required>
-            </div>
-            <div class="textbox">
-                <i class="fa fa-address-card" aria-hidden="true"></i>
-                <input type="text" placeholder="Enter first full name" name="full_name1" required>
-            </div>
-            <div class="textbox">
-                <i class="fa fa-address-card" aria-hidden="true"></i>
-                <input type="text" placeholder="Enter second full name" name="full_name2" required>
-            </div>
-            <div class="textbox">
-                <i class="fa fa-calendar" aria-hidden="true"></i>
-                <input type="date" placeholder="Enter Wedding Date" name="date_of_wedding" required>
-            </div>
-            <div class="textbox">
-                <i class="fa fa-calendar" aria-hidden="true"></i>
-                <input type="time" placeholder="Enter hour of wedding" name="hour_of_wedding" required>
-            </div>
-            <div class="textbox">
-                <i class="fa fa-question-circle" aria-hidden="true"></i>
-                <input type="text" placeholder="What is your budget?" name="budget" required>
-            </div>
-            <input class="btn" type="submit" name="submit" value="Registration">
-        </form>
-        <p> Already have an account? <a href="login.php">Sign-In</a></p>
-    </div>
+        </div>
+    </main>
+
     <!-- added code 9/5/2021 -->
     <?php
     if (isset($_GET["dateError"])) {
@@ -95,6 +135,12 @@ if (isset($_POST['submit'])) {
     }
     ?>
     <!-- end of code -->
+
+
+    <!-- JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </body>
 
 </html>
--- includes/usersManagment/signUpVendor.php
+++ includes/usersManagment/signUpVendor.php
@@ -29,71 +29,121 @@ if (isset($_POST['submit'])) {
 }
 ?>
 <!DOCTYPE html>
-<html>
+
 
 <head>
-    <!-- connection css and bootstrap and metadata -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Registration Form for Vendor</title>
-    <link rel="stylesheet" type="text/css" href="../../css/signup.css">
-    <link rel="shortcut icon" href="../../assets/img/tab_logo.png" type="image/png">
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript">
+    </script>
+    <script src="https://kit.fontawesome.com/90569433a0.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+    <!-- BS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    </script>
+    <!-- BS -->
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    </script>
+    <!-- tab view -->
+    <link rel="shortcut icon" href="../../assets/img/tab_logo.png" type="image/png">
+    <title> Registration Vendor</title>
+    <!-- general fonts-->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,600,700&amp;display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&family=Josefin+Sans:wght@500&family=Niconne&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="../../css/general.css" />
+    <link rel="stylesheet" href="../../css/signUp.css" />
 </head>
 
 <body>
-    <p id="error"><?php echo $error ?></p>
-    <div class="login-box">
-        <h1>Registration - Vendors</h1>
-        <!-- form for Registration  -->
-        <form method="post">
-            <div class="textbox">
-                <i class="fa fa-envelope" aria-hidden="true"></i>
-                <input type="email" placeholder="Example@gmail.com" name="email" required>
+    <main class="d-flex align-items-center min-vh-100 py-3 py-md-0">
+        <div class="container">
+            <div class="card registerForm-card">
+                <div class="row no-gutters">
+                    <div class="col-md-5">
+                        <img src="../../assets/img/signUpVendor.jpg" alt="registerForm" class="registerForm-card-img">
+                    </div>
+                    <div class="col-md-7">
+                        <div class="card-body">
+                            <p class="registerForm-card-description">Registration</p>
+                            <!-- form for log in  -->
+                            <form method="post">
+                                <div class="form-group">
+                                    <input class="form-control" type="email" placeholder="Example@gmail.com"
+                                        name="email" required>
+                                </div>
+                                <div class="form-group mb-4">
+                                    <input class="form-control" type="password" placeholder="password" name="password"
+                                        required>
+                                </div>
+                                <div class="form-group mb-4">
+                                    <input class="form-control" type="text" placeholder="Enter your Company name"
+                                        name="company_name" required>
+                                </div>
+
+                                <div class="form-group mb-4">
+                                    <input class="form-control" type="text" placeholder="phone number"
+                                        pattern="[0-9]{10}" name="phone_num" required>
+                                </div>
+                                <div class="form-group mb-4">
+                                    <select class="form-select" name="kind_of_business" id="kind_of_business">
+                                        <option selected>Select your service</option>
+                                        <option value="Balloons">Balloons</option>
+                                        <option value="DJ">DJ</option>
+                                        <option value="photographer">Photographer</option>
+                                        <option value="Hall design">Hall design</option>
+                                        <option value="Designing tables">Designing tables</option>
+                                        <option value="seating">Seating</option>
+                                        <option value="Catering">Catering</option>
+                                        <option value="Wedding Dresses">Wedding Dresses</option>
+                                        <option value="Makeup">Makeup</option>
+                                        <option value="entertainment show">Entertainment show</option>
+                                        <option value="flowers">Flowers</option>
+                                        <option value="Alcohol">Alcohol</option>
+                                    </select>
+                                </div>
+                                <div class="form-group mb-4">
+                                    <input class="form-control" type="text" placeholder="Enter your web url"
+                                        name="web_url" required>
+                                </div>
+                                <div class="form-group mb-4">
+                                    <input class="form-control" type="text" placeholder="Enter address" name="address"
+                                        required>
+                                </div>
+                                <p id="error"><?php echo $error ?></p>
+                                <input class="btn btn-block registerForm-btn mb-4" type="submit" name="submit"
+                                    value="Registration">
+                            </form>
+                            <p> Already have an account? <a href="login.php">Sign-In</a></p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="textbox">
-                <i class="fa fa-lock" aria-hidden="true"></i>
-                <input type="Password" placeholder="Password" name="password" required>
-            </div>
-            <div class="textbox">
-                <i class="fa fa-address-card" aria-hidden="true"></i>
-                <input type="text" placeholder="Enter your Company name" name="company_name" required>
-            </div>
-            <div class="textbox">
-                <i class="fa fa-user" aria-hidden="true"></i>
-                <input type="text" placeholder="phone number" pattern="[0-9]{10}" name="phone_num" required>
-            </div>
-            <label for="kind_of_business">Choose Kind of Business:</label>
-            <select class="btn" name="kind_of_business" id="kind_of_business">
-                <option value="Balloons">Balloons</option>
-                <option value="DJ">DJ</option>
-                <option value="photographer">Photographer</option>
-                <option value="Hall design">Hall design</option>
-                <option value="Designing tables">Designing tables</option>
-                <option value="seating">Seating</option>
-                <option value="Catering">Catering</option>
-                <option value="Wedding Dresses">Wedding Dresses</option>
-                <option value="Makeup">Makeup</option>
-                <option value="entertainment show">Entertainment show</option>
-                <option value="flowers">Flowers</option>
-                <option value="Alcohol">Alcohol</option>
-            </select>
-            <div class="textbox"></div>
-            <div class="textbox">
-                <i class="fa fa-address-card" aria-hidden="true"></i>
-                <input type="text" placeholder="Enter your web url" name="web_url" required>
-            </div>
-            <div class="textbox">
-                <i class="fa fa-address-card" aria-hidden="true"></i>
-                <input type="text" placeholder="Enter address" name="address" required>
-            </div>
+        </div>
+    </main>
+
+    <!-- added code 9/5/2021 -->
+    <?php
+    if (isset($_GET["dateError"])) {
+        echo "<script> alert('impossible date');</script>";
+    }
+    ?>
+    <!-- end of code -->
 
 
-            <input class="btn" type="submit" name="submit" value="Registration">
-        </form>
-        <p> Already have an account? <a href="login.php">Sign-In</a></p>
-    </div>
+    <!-- JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </body>
 
 </html>
