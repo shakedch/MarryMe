f0014075a90a05f013ce9e984ab7e81e488e88d5
Sapir Koren <sapirke88@gmail.com>
2021-05-20 16:59:11 +0300

    [feat-25] sent email to vendor & changed btn

--- includes/wishList/update_wishList.php
+++ includes/wishList/update_wishList.php
@@ -4,16 +4,30 @@ require_once '../../conection/init.php';
 global $session;
 global $database;
 $id = $_GET['id'];
-$num = 1 ;
-$result=$database->query("UPDATE wishlist SET is_contact_supplier= '".$num."' WHERE whistlist_id = '".$id."'");
-if($result)
-{
-    header("location:wishList.php"); // redirects to all records page
-}
-else
-{
-    echo "Error send email "; // display error message if not update
-	header("location:wishList.php");
-}
+$contact_supplier = $_GET['wishlist'];
+$vendor_email = $_GET['vendor_email'];
+$offer_name = $_GET['offer_name'];
+var_dump($id);
+var_dump($contact_supplier);
+var_dump($vendor_email);
+var_dump($offer_name);
 
-?>
+$num = 1;
+
+if ($id) {
+    if ($contact_supplier == "0") {
+        $result = $database->query("UPDATE wishlist SET is_contact_supplier= '" . $num . "' WHERE whistlist_id = '" . $id . "'");
+        header("Location: mailto: $vendor_email?subject= $offer_name");
+    }
+} else {
+    echo "Error send email "; // display error message if not update
+    header("location:wishList.php");
+}
+   
+
+
+// header("location:wishList.php"); // redirects to all records page
+
+// if ($result) {
+//     header("location:wishList.php"); // redirects to all records page
+// }
